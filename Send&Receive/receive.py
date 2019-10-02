0d385e6b3941ceddab51ed90afe21dabd734c9d6
Yu Zhang <zhangyu@yahoo.com>
2019-10-02 09:47:12 -0500

    Update receive.py

--- Send&Receive/receive.py
+++ Send&Receive/receive.py
@@ -10,12 +10,17 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     global message
+    global count1
     x = msg.payload ##test
     message.append(np.frombuffer(x))
+    if len(message)>count:
+        client.loop_stop()
 
-def receive(server_ip,topic, port, self_name):
+def receive(server_ip,topic, port, self_name,time1,count):
     global mqtt_topic
     global message
+    global count1
+    count1 = count
     message = []
     mqtt_topic = topic
     client = mqtt.Client(self_name)
@@ -23,6 +28,12 @@ def receive(server_ip,topic, port, self_name):
     client.on_message = on_message
     client.connect(server_ip, port, 60)
     client.loop_start()
-    time.sleep(10)
+    time1 = float(time1)
+    time.sleep(time1)
+    exist = len(message)
     client.loop_stop()
     return message
+
+if __name__ == "__main__":
+    x = receive("172.24.6.253","max",12345,"xsdfa",10,3)
+    print(x)
