2504e4b9930c88cdc7473ed7124bfd215ad59eb2
ray-hu <31421543+ray-hu@users.noreply.github.com>
2019-10-03 11:24:30 -0500

    Update local_main.py

--- device/local_main.py
+++ device/local_main.py
@@ -1,92 +1,42 @@
 import tensorflow as tf
 import numpy as np
-from protocol.protocol_utils import worker_uplaod, worker_receive
+from protocol_utils import receive, send
+from model import Model
 
-def batch_data(data, batch_size):
-    '''
-    data is a dict := {'x': [numpy array], 'y': [numpy array]} (on one client)
-    returns x, y, which are both numpy array of length: batch_size
-    '''
-    data_x = data['x'] ## modify
-    data_y = data['y']
-
-    # randomly shuffle data
-    np.random.seed(100)
-    rng_state = np.random.get_state()
-    np.random.shuffle(data_x)
-    np.random.set_state(rng_state)
-    np.random.shuffle(data_y)
-
-    # loop through mini-batches
-    for i in range(0, len(data_x), batch_size):
-        batched_x = data_x[i:i+batch_size]
-        batched_y = data_y[i:i+batch_size]
-        yield (batched_x, batched_y)
-
-
-def local_solver(model_params=None):
-    # load local dataset
-    train_X = 0
-    train_y = 0
-    test_x = 0
-    test_y = 0
-    data_train = []
-
-
-    # Parameters
-    learning_rate = 0.01
-    epochs = 25
-    batch_size = 10
-    display_step = 1
-    num_classes = 10
-    regu_param = 1e-3
-
-    # initialize graph
-    graph = tf.Graph()
-    with graph.as_default():
-        # tf Graph Input
-        X = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
-        y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
-
-        logits = tf.layers.dense(inputs=X, units=num_classes, kernel_regularizer=tf.contrib.layers.l2_regularizer(regu_param))
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-
-        predictions = {
-                    "classes": tf.argmax(input=logits, axis=1),
-                    "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-                    }
-
-        loss = tf.losses.sparse_softmax_cross_entropy(labels=y, logits=logits)
-        grads_and_vars = optimizer.compute_gradients(loss)
-        train_op = optimizer.apply_gradients(grads_and_vars, global_step=tf.train.get_global_step())
-
-        eval_metric_ops = tf.count_nonzero(tf.equal(y, predictions["classes"]))
-        saver = tf.train.Saver()
-    sess = tf.Session(graph=graph)
-    # initalization
-
-    if model_params is not None:
-        with graph.as_default():
-            all_vars = tf.trainable_variables()
-            for variable, value in zip(all_vars, model_params):
-                variable.load(value, sess)
-    
-    # train
-    for epoch in range(epochs):
-        for batch_X, batch_y in batch_data(data_train, batch_size):
-            with graph.as_default():
-                sess.run(train_op, feed_dict={X: batch_X, y: batch_y})
-    soln =  sess.run(tf.trainable_variables())
-    return soln
 
 def main():
-    # receive model_params from server
-    model_params = receive('ip of server')
-    
-    # calculate local model
-    local_model = local_solver(model_params)
-    # upload local model to server
-    upload(local_model, 'ip of server')
+
+    # id of this device
+    ID = "01"
+
+    # define local model
+    optimizer = tf.train.GradientDescentOptimizer(0.1)
+    model = Model(num_classes=10, optimizer=optimizer, regu_param=1e-3)
+
+    # num of communication round
+    num_rounds = 100
+    # num of local iterations
+    num_epochs = 10
+
+    # receive dataset from server
+    topic = "data_" + ID
+    dataset = receive("172.24.6.253", topic=topic, 12345, self_name=None,time1=None,count=1)
+
+    for round in range(num_rounds):
+        # receive model parameter from server
+        model_params = receive("172.24.6.253", topic="global_model", 12345, self_name=None,time1=None,count=1)
+        # check if received global model
+        if len(model_params) == 0:
+            print('='*60)
+            print('[INFO] DID NOT RECEIVE GLOBAL MODEL !')
+            return 0
+        
+        # update local model
+        local_model = model.solve_inner(data=dataset, num_epochs=num_epochs, batch_size=10)
+
+        # upload local model to server
+        send("172.24.6.253", "local_model", 12345, local_model)
+        
 
 if __name__ == '__main__':
     main()
